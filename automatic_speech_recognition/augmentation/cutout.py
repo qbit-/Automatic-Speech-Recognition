9e0c227078b0f422574b3e4b5f44df3df80752d1
qbit- <qbit271@protonmail.com>
2020-06-24 22:49:42 +0300

    modified cutout

--- automatic_speech_recognition/augmentation/cutout.py
+++ automatic_speech_recognition/augmentation/cutout.py
@@ -4,59 +4,51 @@ import numpy as np
 class Cutout:
 
     def __init__(self,
-                 prob: float = 0.5,
-                 area_frac: float = 0.02,
-                 aspect_ratio: float = 1 / 0.3,
-                 fill_value: float = None,
+                 F: int = None,
+                 T: int = None,
+                 n: int = None,
+                 fill_value: float = 0,
                  seed: int = 1):
         """
         Cutout augmentation replaces a random rectangular region of the
         spectrogram with mean value / fill value
 
-        :param prob: probability of augmentation, default 0.5
-        :param area_frac: fraction of the spectrogram to cut
-        :param aspect_ratio: aspect ratio (time / frequencies)
+        :param F: maximal range of frequencies to be altered
+        :param T: maximal timestep to be altered at random
+        :param m: number cutout regions
         :param fill_value: fill value. Default None, which means the cut
                            is filled with average value
         :param seed: random seed
         """
-        self.prob = prob
-        self.area_frac = area_frac
-        self.aspect_ratio = aspect_ratio
+        self.F = F
+        self.T = T
+        self.n = n
         self.fill_value = fill_value
         np.random.seed(seed)
 
     def __call__(self, batch_features: np.ndarray,
                  feature_lengths: np.ndarray) -> np.ndarray:
         return np.stack(
-            [self.cut_features(features, feature_length)
+            [self.cut_features(features, feature_length,
+                               self.F, self.T, self.n, self.fill_value)
              for features, feature_length in
              zip(batch_features, feature_lengths)], axis=0)
 
-    def cut_features(self, features: np.ndarray, feature_length: int):
-        pval = np.random.rand()
-        if pval > self.prob:
-            return features
-
+    def cut_features(features: np.ndarray, feature_length: int, F: int,
+                     T: int, n_cuts: int, fill_value: float = None):
         features = features.copy()
         _, channels = features.shape
         time = feature_length
 
-        area = time * channels * self.area_frac
-        time_cut = int(np.sqrt(area * self.aspect_ratio))
-        freq_cut = int(np.sqrt(area / self.aspect_ratio))
+        for i in range(n_cuts):
+            t = np.random.random_integers(low=0, high=T)
+            t0 = np.random.random_integers(low=0, high=time-T)
 
-        freq_min = np.random.random_integers(
-            low=0, high=channels-freq_cut)
-        time_min = np.random.random_integers(low=0, high=time-time_cut)
+            f = np.random.random_integers(low=0, high=F)
+            f0 = np.random.random_integers(low=0, high=channels-F)
 
-        if self.fill_value:
-            fill_value = self.fill_value
-        else:
-            fill_value = np.mean(features[
-                time_min:time_min+time_cut, freq_min:freq_min+freq_cut]
-            )
+            if not fill_value:
+                fill_value = np.mean(features[t0:t0+t, f0:f0+f])
 
-        features[time_min:time_min+time_cut,
-                 freq_min:freq_min+freq_cut] = fill_value
+            features[t0:t0+t, f0:f0+f] = fill_value
         return features
