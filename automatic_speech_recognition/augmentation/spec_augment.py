9384d7004d6023559806edcf6d54e4042edc0033
qbit- <qbit271@protonmail.com>
2020-06-24 19:21:49 +0300

    added fill value to spec augment

--- automatic_speech_recognition/augmentation/spec_augment.py
+++ automatic_speech_recognition/augmentation/spec_augment.py
@@ -10,19 +10,23 @@ class SpecAugment:
                  T: int = None,
                  max_p: float = 1.0,
                  mt: int = None,
+                 fill_value: float = None,
                  seed: int = 1):
         """ SpecAugment: A Simple Data Augmentation Method.
         This augmentation replaces rectangular regions of the
         spectrogram (across all timesteps or all frequencies)
         by the mean value across all timesteps.
 
-        TODO: implement sprse_image_warp to reproduce the original SpecAugment paper
+        TODO: implement sparse_image_warp to reproduce the
+              original SpecAugment paper
 
         :param F: maximal range of frequencies to be altered
         :param mf: number of frequency alterations
         :param T: maximal timestep to be altered at random
         :param max_p: upper bound on the timestep: fraction of total timesteps
         :param mt: number of timestep alterations
+        :param fill_value: fill value for the masked region.
+                           If None, than mean frequency over timesteps is used
         :param seed: random seed
         """
         self.F = F
@@ -30,6 +34,7 @@ class SpecAugment:
         self.T = T
         self.mt = mt
         self.max_p = max_p
+        self.fill_value = fill_value
         np.random.seed(seed)
 
     def __call__(self, batch_features: np.ndarray,
@@ -45,31 +50,35 @@ class SpecAugment:
         _, channels = features.shape
         time = feature_length
 
-        # The mean should be zero if features are normalized
-        means = features.mean(axis=0)
+        if not self.fill_value:
+            # The mean should be zero if features are normalized
+            fill = features.mean(axis=0)
+        else:
+            fill = np.zeros(features.shape[1])
+
         if self.F and self.mf:
             features = self.mask_frequencies(
-                features, means, channels, self.F, self.mf)
+                features, fill, channels, self.F, self.mf)
         if self.T and self.mt:
             features = self.mask_time(
-                features, means, time, self.T, self.mt, self.max_p)
+                features, fill, time, self.T, self.mt, self.max_p)
         return features
 
     @staticmethod
-    def mask_frequencies(features: np.ndarray, means: np.ndarray,
+    def mask_frequencies(features: np.ndarray, fill: np.ndarray,
                          channels: int, F: int, mf: int):
         for i in range(mf):
             f = np.random.random_integers(low=0, high=F)
             f0 = np.random.random_integers(low=0, high=channels-F)
-            features[:, f0:f0+f] = means[f0:f0+f]
+            features[:, f0:f0+f] = fill[f0:f0+f]
         return features
 
     @staticmethod
-    def mask_time(features: np.ndarray, means: np.ndarray,
+    def mask_time(features: np.ndarray, fill: np.ndarray,
                   time: int, T: int, mt: int, max_p: float = 1.0):
         for i in range(mt):
             t = int(np.clip(np.random.random_integers(low=0, high=T),
                             a_min=0, a_max=max_p * time))
             t0 = np.random.random_integers(low=0, high=time-T)
-            features[t0:t0+t, :] = means
+            features[t0:t0+t, :] = fill
         return features
