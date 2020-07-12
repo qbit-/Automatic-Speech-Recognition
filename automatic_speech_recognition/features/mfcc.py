2bd960793d8bf141eeac3405e7867aa4c826fcb5
qbit- <qbit271@protonmail.com>
2020-07-13 02:52:55 +0300

    added legacy mfcc

--- automatic_speech_recognition/features/mfcc.py
+++ automatic_speech_recognition/features/mfcc.py
@@ -1,6 +1,7 @@
 import math
 import numpy as np
 import librosa
+from tensorflow.python.ops import gen_audio_ops as contrib_audio
 from . import audio_utils
 from .. import features
 
@@ -51,3 +52,38 @@ class MFCC(features.FeaturesExtractor):
 
         # put features into correct order (time, n_features)
         return features.transpose()
+
+
+class MFCC_legacy(features.FeaturesExtractor):
+    """
+    This class calculates the Mel-frequency Cepstral Coefficients (MFCCs)
+    This version works for pretrained weights of DeepSpeech from
+    Mozilla
+    """
+    def __init__(self, features_num: int,
+                 standardize=False,
+                 sample_rate=16000,
+                 winlen=0.02, winstep=0.01):
+        self.features_num = features_num
+        self.sample_rate = sample_rate
+        self.window_size = int(winlen * sample_rate)
+        self.window_step = int(winstep * sample_rate)
+        super().__init__(standardize=standardize)
+
+    def make_features(self, audio: np.ndarray) -> np.ndarray:
+        """Use Tensorflow  lib to extract
+           log filter banks from the features file. """
+        audio = audio[:, np.newaxis]
+        spectrogram = contrib_audio.audio_spectrogram(
+            audio,
+            window_size=self.window_size,
+            stride=self.window_step,
+            magnitude_squared=True)
+        mfccs = contrib_audio.mfcc(
+            spectrogram=spectrogram,
+            sample_rate=self.sample_rate,
+            dct_coefficient_count=self.features_num,
+            upper_frequency_limit=8000)
+
+        # take the first channel only
+        return mfccs[0]
